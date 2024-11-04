25deac47d4e14ca63d74ece6cc56bf9cc9ee32b8
Prasun <maitypro402@gmail.com>
2024-11-05 01:53:41 +0530

    Update title_maker.py

--- title_maker.py
+++ title_maker.py
@@ -1,22 +1,22 @@
-import os
 import streamlit as st
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 from huggingface_hub import login
+from Hugginface_prasun import hf_read_access_key
 
 # Log in to Hugging Face
 login(hf_read_access_key)
 
 # Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained("Prasun21/Abstract-to-title")
+tokenizer = AutoTokenizer.from_pretrained("./llama_1B_lora_finetuned")
 
 # Load the base LLaMA model from Hugging Face
 base_model_path = "meta-llama/Llama-3.2-1B"  # Update to the model's Hugging Face ID
 base_model = AutoModelForCausalLM.from_pretrained(base_model_path)
 
 # Load the LoRA fine-tuned model
-model = PeftModel.from_pretrained(base_model, "Prasun21/Abstract-to-title")
+model = PeftModel.from_pretrained(base_model, "./llama_1B_lora_finetuned")
 
 def generate_title(abstract, temperature, top_k, top_p):
     prompt_template = f"Generate a short title by reading the following abstract:\n\nAbstract: {abstract}\n\nTitle: "
